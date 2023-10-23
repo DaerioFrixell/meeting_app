fc369644cd103284a1e4f6cc63ec8f29c84f9104
denis <anywaymaybe1@gmail.com>
2023-10-23 20:32:43 +0400

    015/Feature: add unitFields array

--- src/components/unitList/fields.ts
+++ src/components/unitList/fields.ts
@@ -1,26 +1,27 @@
 export const unitFields = [
   {
-    id: "id",
-    number: "№",
+    id: 'id',
+    number: '№',
     status: {
-      number: "№",
-      metric: "metric",
-      mark: "status mark"
+      title: 'status',
+      number: '№',
+      metric: 'metric',
+      mark: 'status mark',
     },
-    name: "name",
-    surename: "surename",
-    wasOld: "was old",
-    dateMeet: "date meet",
+    name: 'name',
+    surename: 'surename',
+    wasOld: 'was old',
+    dateMeet: 'date meet',
     links: {
-      title: "links",
-      vk: "vk",
-      inst: "inst",
-      telegram: "tg",
-      otherLink: "other link"
+      title: 'links',
+      vk: 'vk',
+      inst: 'inst',
+      telegram: 'tg',
+      otherLink: 'other link',
     },
-    whereMeet: "where meet",
-    typeMeet: "type meet",
-    createAt: "created",
-    updateAt: "updated"
-  }
+    whereMeet: 'where meet',
+    typeMeet: 'type meet',
+    createAt: 'created',
+    updateAt: 'updated',
+  },
 ]
